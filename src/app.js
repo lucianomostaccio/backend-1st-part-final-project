51411829830b7162a4f5f4dbacdae29f38c7e3be
Luciano Mostaccio <lucianomostaccio@gmail.com>
2023-11-12 15:03:33 -0300

    moved productmanager into src folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const ProductManager = require("../ProductManager.js");
+const ProductManager = require("./ProductManager.js");
 
 const app = express();
 const port = 8080;
